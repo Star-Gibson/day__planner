c13b77dd4ca77cee3b35a2a8d9e3a32946db63c9
Star-Gibson <sgibsoncoding@gmail.com>
2020-11-11 21:39:33 -0500

    Functions working as planned in console, still having page refresh issues

--- planner.js
+++ planner.js
@@ -5,7 +5,7 @@ currentDay.text(now.format("dddd - MMMM Do, YYYY")); //Should Dislay the Date in
 //Defined variable for easier use in Button Click/Hover/Save functions. 
 var saveBtn = $(".saveBtn")
 
-highlight(); 
+highlight();
 
 function highlight() { //Function which uses the css classes to appropriately highlight the textarea based off of time of the day.
     var hour = moment().hours(); //MomentJS Hours function.
@@ -24,11 +24,12 @@ function highlight() { //Function which uses the css classes to appropriately hi
     })
 }
 
-//Routine Set Function (LocalStorage)
+//Routine Set Function (LocalStorage) - Should keep data from localStorage in textarea fields upon refresh.
 $(".time-block").each(function () {
     var id = $(this).attr("id");
-    console.log($(this).attr("id"));
+    console.log($(this).attr("id")); 
     var routine = localStorage.getItem(id);
+    console.log(routine);
 
     if (routine !== null) {
         $(this).children("routine").val(routine);
@@ -36,18 +37,33 @@ $(".time-block").each(function () {
 
 });
 
+
+//Button Save (localStorage) //Saves all data to localStorage in the following format - 9AM | Do homework
+saveBtn.on("click", function () {
+    var currentTime = $(this).parent().attr("id")
+    var routine = $(this).siblings(".routine").val();
+    localStorage.setItem(currentTime, routine);
+
+}
+
+
+)
+
+
 // Button Hover
-    
-    saveBtn.on("mouseenter", function () {
-        $(this).addClass("hover");
-    });
 
-    saveBtn.on("mouseleave", function () {
-        $(this).removeClass("hover");
-    });
+saveBtn.on("mouseenter", function () {
+    $(this).addClass("hover");
+});
+
+saveBtn.on("mouseleave", function () {
+    $(this).removeClass("hover");
+});
 
 
-//Button Save
+
+
+
 
 
 
